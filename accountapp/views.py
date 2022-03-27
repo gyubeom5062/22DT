b8f1be42a28cb126c2f13bca68b0786c3a2a8cbd
master <kgb6346@naver.com>
2022-03-27 16:20:03 +0900

    django course 18 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -15,8 +17,12 @@ def hello_world(request):
         # 실제 db에 헬로월드 객체 저장
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        hello_world_list = HelloWorld.objects.all()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        #return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET_METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
     # return HttpResponse('hello world')
