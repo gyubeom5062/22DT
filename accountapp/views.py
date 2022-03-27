e5d0af2a29939f1194d48769c595b3f83889c5f8
master <kgb6346@naver.com>
2022-03-27 15:42:11 +0900

    django course 17 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,14 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
     if request.method == "POST":
-        return  render(request, 'accountapp/hello_world.html', context={'text': 'POST_METHOD!!!'})
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        # 실제 db에 헬로월드 객체 저장
+        new_hello_world.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET_METHOD!!!'})
 
-    #return HttpResponse('hello world')
-
+    # return HttpResponse('hello world')
