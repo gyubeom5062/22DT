1de875d8252fb84fcea4435d808e0d36fced822e
master <kgb6346@naver.com>
2022-03-27 17:07:55 +0900

    django course 21 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+    #as_view()를 적어줘야 함수형 헬로월드처럼 반응함.
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -26,3 +29,11 @@ def hello_world(request):
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
     # return HttpResponse('hello world')
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    #reverse와 차이는 리버스는 함수와, 레이지는 클래스와 사용
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
