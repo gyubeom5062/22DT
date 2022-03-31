c48d1db171b3e004eeecf903f2253a31c09f0e1d
gyubeom5062 <gyubeom5062@gmail.com>
2022-03-31 16:17:32 +0900

    django course 29 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -157,3 +157,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+
+MEDIA_URL = '/media/'
+# ex) 127.0.0.1:8000/media/test.jpg
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
